6dbcccaa15026a87653fea4b876d97ad99829812
JuanK <camiloalzatebedoya15@gmail.com>
2025-11-05 19:51:46 -0500

    feat(back) nombre de la tabla en la BD
    Establecer nombre especifico en la base de datos

--- backend/backend-libreria/src/main/java/com/backend/libreria/libros/entity/Libro.java
+++ backend/backend-libreria/src/main/java/com/backend/libreria/libros/entity/Libro.java
@@ -1,13 +1,11 @@
 package com.backend.libreria.libros.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "libros")
 public class Libro {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
